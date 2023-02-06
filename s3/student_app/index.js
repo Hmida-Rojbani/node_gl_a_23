6c8fb9a63fe72f4a57a760816801b6da5475bb79
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2023-02-06 11:37:17 +0100

    config

--- s3/student_app/index.js
+++ s3/student_app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const appDebugger= require('debug')('app:students:Debug')
 const student_router = require('./routers/students')
 const Welcome_router = require('./routers/welcome')
+const config = require('config');
 const morgan = require('morgan')
 const app = express()
 const port = 3000;
 app.use(express.json())
 appDebugger('Node Env :',app.get('env'));
+appDebugger('Application name :',config.get('app.name'))
+appDebugger('Application version :',config.get('app.version'))
+appDebugger('DB :',config.get('DB'))
 //console.log('Node Env :',process.env.NODE_ENV);
 if(app.get('env')==='development')
     app.use(morgan('dev'))
