7ae4ba33835327751b927e1c8ae006fccda9c470
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2023-02-13 11:31:49 +0100

    add student model

---
+++ s5/models/student.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const student_schema = new mongoose.Schema({
+    name : String,
+    inscriptionDate : {
+        type: Date,
+        default : Date.now()
+    },
+    age: Number,
+    active: Boolean,
+    payedAmount : Number
+});
+
+const Student = mongoose.model('Student',student_schema);
+
+module.exports.Student= Student;
